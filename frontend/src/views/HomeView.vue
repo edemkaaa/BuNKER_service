3651756cc69af452832af65ed26fc5e0cd0e3212
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 13:31:38 +0300

    Подключение объявлений из  store

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <v-container>
     <v-row justify="center">
       <v-col cols="12" xs="12">
-        <v-carousel cycle interval="5000">
+        <v-carousel>
           <v-carousel-item
-          v-for="ad in ads"
+          v-for="ad in promoAds"
           :key="ad.id"
           :src="ad.src"
           cover>
@@ -44,59 +44,12 @@
 
 <script>
 export default {
-  data() {
-    return {
-      ads: [
-        {
-          title: "Ремонт вмятин",
-          desc: "First Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-01.jpg",
-          id: "1"
-        },
-        {
-          title: "Оклейка авто плёнкой",
-          desc: "Second Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-08.jpg",
-          id: "2"
-        },
-        {
-          title: "Кузовной ремонт",
-          desc: "Thitd Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-11.jpg",
-          id: "3"
-        },
-        {
-          title: "Реставрация кожаного салона",
-          desc: "Fourth Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-05.jpg",
-          id: "4"
-        },
-        {
-          title: "Покраска",
-          desc: "Fifth Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-09.jpg",
-          id: "5"
-        },
-        {
-          title: "Удаление прожёгов",
-          desc: "Sixth Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-13.jpg",
-          id: "6"
-        },
-        {
-          title: "Полировка кузова авто",
-          desc: "Seventh Desc",
-          promo: true,
-          src: "https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-14.jpg",
-          id: "7"
-        }
-      ]
+  computed: {
+    promoAds() {
+      return this.$store.getters.promoAds
+    },
+    ads() {
+      return this.$store.getters.ads
     }
   }
 }
