e0c26d1af7a07f8c66800cc1e3c2fb3f8ba836d6
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 12:50:32 +0300

    Верстка страницы детального просмотра объявления

--- frontend/src/views/Ads/AdView.vue
+++ frontend/src/views/Ads/AdView.vue
@@ -1,16 +1,39 @@
 <template>
     <v-container>
-      <v-row justify="center">
-        <h1>Ad</h1>
-      </v-row>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="mt-5">
+                    <v-img height="300px" src="https://автоцарапина.рф/images/grey/overcontent-slider/overcontent-01.jpg"></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary mb-3">Удаление вмятин</h1>
+                        <p>
+                            Сервисный центр «BuNKER» в Крыму проводит работы по 
+                            удалению мелких и глубоких вмятин на автомобилях разных 
+                            марок, моделей и годов выпуска. Мы предлагаем наиболее 
+                            эффективные решения по восстановлению кузова в каждом 
+                            конкретном случае, используем передовые технологические 
+                            разработки и качественные расходные материалы известных 
+                            мировых производителей, предоставляем гарантию и обеспечиваем 
+                            доступные цены. Профессионально выполним рихтовку вмятин на 
+                            двери, багажнике, бампере, капоте, крыше и на других 
+                            элементах кузова автомобиля без покраски.
+                        </p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning" color="orange">Редактировать</v-btn>
+                        <v-btn class="success" color="green">Записаться</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
-  </template>
-  
-  <script>
-  export default {
-    data () {
-      return {
-      }
-    }
-  }
-  </script>
+</template>
+
+<script>
+export default {
+    data() {
+        return {};
+    },
+};
+</script>
