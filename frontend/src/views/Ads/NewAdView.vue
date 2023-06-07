a3d5f544f7ee0873b6a0164a1876a472ef3bae8f
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 13:38:02 +0300

    Подготовка данных объявления

--- frontend/src/views/Ads/NewAdView.vue
+++ frontend/src/views/Ads/NewAdView.vue
@@ -49,15 +49,16 @@ export default {
         };
     },
     methods: {
-        createAd() {
-            if (this.$refs.form.validate()) {
-                const ad = {
-                    title: this.title,
-                    desc: this.description,
-                    promo: this.promo,
-                };
-                console.log(ad);
-            }
+        createAd(){
+if (this.$refs.form.validate()){
+		const ad = {
+		title: this.title,
+		desc: this.description,
+		promo: this.promo,
+		src: "https://cdn.vuetifyjs.com/images/cards/cooking.png"
+		}
+		console.log(ad)
+	}
         },
     },
 };
