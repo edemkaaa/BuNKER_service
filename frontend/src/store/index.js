2415df994ef8a38e478440be7c7d217c7ca307f0
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 14:06:57 +0300

    Создание модуля  store для сопровождения запросов

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,12 @@
 import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
+import shared from './shared'
 
 
 const store = createStore({
 modules:{
-ads, user
+ads, user, shared
 }
 })
 
