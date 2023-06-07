8ae0eb04f07dda7de2dad6d050f1b43995404d9a
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 14:02:38 +0300

    Создание action  и  mutation  для регистрации пользователя

--- frontend/src/store/user.js
+++ frontend/src/store/user.js
@@ -1,12 +1,30 @@
+class User {
+    constructor(id, email, password) {
+        this.id = id
+        this.email = email
+        this.password = password
+    }
+}
+
 export default {
 	state: {
         user: null
     },
-	mutations: {},
-	actions: {},
+	mutations: {
+        setUser(state,payload) {
+        console.log(payload)
+        state.user = payload
+        }
+    },
+    actions: {
+        registerUser({commit},{email, password}){
+        //Здесь запрос на сервер для регистрации
+        commit('setUser', new User(1, email, 	password))
+        }
+    },
 	getters: {
-      user(state) {
-        return state.user
+        user(state) {
+            return state.user
 	}
-    }
+}
 }
