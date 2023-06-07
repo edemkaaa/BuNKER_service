7eb1f7b06c966a34214ce058223c1cf45f5822bf
edemkaaa <edemasanov2004@gmail.com>
2023-06-07 22:21:18 +0300

    Рефакторинг метода createAd

--- frontend/src/store/ads.js
+++ frontend/src/store/ads.js
@@ -60,33 +60,58 @@ export default {
         ]
     },
     mutations: {
-        createAd(state, payload){
+        createAd(state, payload) {
             state.ads.push(payload)
         }
     },
     actions: {
-        createAd({commit},payload){
-        payload.id = Math.random()
-            commit('createAd', payload)
+        createAd({ getters }, payload) {
+            payload.id = Math.random()
+            payload.userId = getters.user != null ? getters.user.id : '1'
         }
-    
+
     },
     getters: {
         ads(state) {
-                return state.ads
-            },
-            promoAds(state) {
-                return state.ads.filter(ad => {
-                    return ad.promo
-                })
-            },
-            myAds(state) {
-                return state.ads
-            },
-            adById(state) {
-                return id => {
-                    return state.ads.find(ad => ad.id == id)
+            return state.ads
+        },
+        promoAds(state) {
+            return state.ads.filter(ad => {
+                return ad.promo
+            })
+        },
+        myAds(state) {
+            return state.ads
+        },
+        adById(state) {
+            return id => {
+                return state.ads.find(ad => ad.id == id)
+            }
         }
+    },
+    async createAd({ commit, getters }, payload) {
+        payload.id = Math.random()
+        payload.userId = getters.user != null ? getters.user.id : '1'
+        commit('clearError')
+        commit('setLoading', true)
+        //Заглушка запроса
+        let isRequestOk = true
+        let promise = new Promise(function (resolve) {
+            setTimeout(() => resolve('Done')
+                , 3000);
+        });
+        if (isRequestOk) {
+            await promise.then(() => {
+                commit('createAd', payload)
+                commit('setLoading', false)
+            })
+        } else {
+            await promise.then(() => {
+                commit('setLoading', false)
+                commit('setError', 'Ошибка создания объявления')
+                throw 'Упс... Ошибка создания объявления'
+            })
+        }
+
     }
-}
 }
